8b3c6bb817d1655d7bc1a3e985e2107cd62ebb64
Suryavanshi11444 <modi00447@gmail.com>
2025-02-27 19:39:05 +0530

    save all changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <nav className="bg-white shadow-md p-4">
+    <nav className="bg-white shadow-md p-4 sticky top-0 z-50">
       <div className="container mx-auto flex justify-between items-center">
         {/* Logo Image */}
         <Link to="/">
